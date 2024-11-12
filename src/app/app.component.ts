f2bdd81e31f0ddc1ab6fb617e9a5129a343f59e1
Mbali-Nyalungu <Mbali.Nyalungu04@gmail.com>
2024-11-12 13:37:51 +0200

    created loader

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,47 @@
+// import { Component } from '@angular/core';
+// import { RouterOutlet } from '@angular/router';
+// import { LoadComponent } from "./load/load.component";
+// import { DataComponent } from "./data/data.component";
+
+// @Component({
+//   selector: 'app-root',
+//   standalone: true,
+//   imports: [RouterOutlet, LoadComponent, DataComponent],
+//   templateUrl: './app.component.html',
+//   styleUrl: './app.component.css'
+// })
+// export class AppComponent {
+//   title = 'app';
+
+//   isLoading = true;
+//   constructor() {
+//     setTimeout(() => {
+//       this.isLoading = false
+//     }, 3000);
+//   }
+// }
+
+
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { LoadComponent } from "./load/load.component";
 import { DataComponent } from "./data/data.component";
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, LoadComponent, DataComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'load';
-
+  title = 'app';
   isLoading = true;
+
   constructor() {
+    
     setTimeout(() => {
-      this.isLoading = false
+      this.isLoading = false; 
     }, 3000);
   }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { LoadComponent } from './load/load.component';
 
 export const routes: Routes = [
+    {
 
-   { path:'',  component:LoadComponent}
+   path:'' ,
+   component:LoadComponent}
 ];
